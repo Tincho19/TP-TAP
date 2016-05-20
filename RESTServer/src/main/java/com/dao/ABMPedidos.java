96d1dad4d6f8313ff816c23e1ef65b9506e27e9e
Tincho <Tincho@Tincho-T420>
2016-05-20 20:11:59 -0300

    20/05 - Base de datos ok

--- RESTServer/src/main/java/com/dao/ABMPedidos.java
+++ RESTServer/src/main/java/com/dao/ABMPedidos.java
@@ -13,14 +13,37 @@ import com.entities.Pedido;
 public class ABMPedidos{
 	
 	Connection conn = null;
-	
+	 static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	 static final String DB_URL = "jdbc:sqlserver://TINCHO-T420:1032//TAPDataBase";
+
+	   //  Database credentials
+	   static final String USER = "sa";
+	   static final String PASS = "a66179";
+	   
 	private ABMPedidos(){
 		try {
-			Class.forName("org.hsqldb.jdbcDriver");
-			conn = DriverManager.getConnection("jdbc:hsqldb:C:/TP-TAP/TablaPedidos.db", "sa", "");
+			
+			
+//				Class.forName("org.hsqldb.jdbcDriver");
+//				conn = DriverManager.getConnection("jdbc:hsqldb:C:/TP-TAP/TablaPedidos.db", "sa", "");
+				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+				String server = "TINCHO-T420\\SQLEXPRESS2012:1032";
+				String user = "sa";
+				String pass = "a66179";
+				String dataBase = "TAPDataBase";
+	//			String jdbcUrl = ("jdbc:sqlserver//"+server);
+				String jdbcUrl = ("jdbc:sqlserver://"+server+";user="+user+";password="+pass+";databaseName="+dataBase);
+				conn = DriverManager.getConnection(jdbcUrl);
+				generarReporte();
+				   
+//				   Statement stmt = null;
+//				      //STEP 2: Register JDBC driver
+//					Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//				      //STEP 3: Open a connection
+//				      conn = DriverManager.getConnection(DB_URL,USER,PASS);
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -104,17 +127,15 @@ public class ABMPedidos{
 		String Resultado = "";
 		try {
 			stmt = conn.createStatement();
-			ResultSet resultSet = stmt.executeQuery("SELECT * FROM pedido;");
+			ResultSet resultSet = stmt.executeQuery("SELECT * FROM pedidos;");
 			if(null != resultSet){
 				while(resultSet.next()){
 					String id = resultSet.getString("id");
 					String nombre = resultSet.getString("nombre");
-					String calle = resultSet.getString("calle");
-					String altura = resultSet.getString("altura");
-					String piso = resultSet.getString("piso");
+					String direccion = resultSet.getString("direccion");
 					String telefono = resultSet.getString("telefono");
 					String comida = resultSet.getString("comida");
-					Resultado += (" // "+id+" "+nombre+" "+calle+" "+altura+" "+piso+" "+telefono+" "+comida);
+					Resultado += (" // "+id+" "+nombre+" "+direccion+" "+telefono+" "+comida);
 				}
 			}else{
 				System.out.println("no se encontraron resultados.");
--- RESTServer/src/main/java/com/db/DBTest.java
+++ RESTServer/src/main/java/com/db/DBTest.java
@@ -1,22 +1,23 @@
 package com.db;
 
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
  
 public class DBTest {
  
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");	
-		String dbURL = "jdbc:sqlserver://127.0.0.1/TAPDataBase;integratedSecurity=true;";
+		
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		
+		String server = "TINCHO-T420\\SQLEXPRESS2012:1032";
 		String user = "sa";
 		String pass = "a66179";
-		Connection conn = DriverManager.getConnection(dbURL, user, pass);
+		String dataBase = "TAPDataBase";
+		String jdbcUrl = ("jdbc:sqlserver//"+server);
+//		String jdbcUrl = ("jdbc:sqlserver//"+server+";user="+user+";password="+pass+";databaseName="+dataBase);
+		Connection conn = DriverManager.getConnection(jdbcUrl,user,pass);
 		System.out.println("test");
 		Statement sta = conn.createStatement();
-		String Sql = "select * from pedidos";
+		String Sql = "select * from Pedidos";
 		ResultSet rs = sta.executeQuery(Sql);
 		while (rs.next()) {
 			System.out.println(rs.getString("txt_title"));
